210e41dadf2df9b37ff94224177bc34ea87c4ae8
Ustinova <rykoca.tanya.96@gmail.com>
2024-10-28 18:45:52 +0300

    кынун

--- mock-api.js
+++ mock-api.js
@@ -1,5 +1,5 @@
 const { KinopoiskDev } = require('@openmoviedb/kinopoiskdev_client');
-const fs = require('fs'); // Импортируем модуль fs для работы с файловой системой
+
 
 const TOKEN = 'FC7DN76-PDS41TM-KDFFT9W-1CS9TS9';
 
@@ -14,62 +14,67 @@ const bodyParser = require('body-parser');
 const { request } = require('http');
 
 const handlers = {
-    // ['/favorites']: {
-    //     GET: (request, response) => {
-    //         setTimeout(() => response.json(response.locals.data), 20000);
-    //     }
-    // },
-    ['/movies']: {
-        POST: async (request, response) => {
-           
-                const res = await kp.movie.getByFilters(request.body);
+  // ['/favorites']: {
+  //     GET: (request, response) => {
+  //         setTimeout(() => response.json(response.locals.data), 20000);
+  //     }
+  // },
+  ['/movies']: {
+    POST: async (request, response) => {
 
-                response.status(200).send(res);
-           
-        },
-        GET: async (request, response) => {
-            const { id } = request.query;
-            // Фильтрация по id
-            const res = await kp.movie.getById(id);
-            response.status(200).send(res)
-        },
+      const res = await kp.movie.getByFilters(request.body);
+
+      response.status(200).send(res);
+
     },
-    ['/users']: {
-        GET: async (request, response) => {
-            const user = response.locals.data;
-            response.status(200).send(user[0] || null)
-        }
+    GET: async (request, response) => {
+      const { id } = request.query;
+      // Фильтрация по id
+      const res = await kp.movie.getById(id);
+      response.status(200).send(res)
     },
-    ['/genres']: {
-        GET: async (_, response) => {
-            const res = await kp.movie.getPossibleValuesByField('genres.name');
-            response.status(200).send(res);
-        }
+  },
+  ['/users']: {
+    GET: async (request, response) => {
+      const user = response.locals.data;
+      response.status(200).send(user[0] || null)
+    },
+    // POST:async (request, response)=>{
+    //   const {users}=response.query;
+    //   const exisingUser = users.find(user=>user)
+    // }
+  },
+  ['/possible-filter-values']: {
+    GET: async (request, response) => {
+      const { field } = request.query;
+      const res = await kp.movie.getPossibleValuesByField(field);
+      response.status(200).send(res);
     }
+  }
 }
 
 
 
 router.render = (request, response) => {
-    const { method, path } = request
+  const { method, path } = request
 
-    const handler = handlers[path]?.[method]
+  const handler = handlers[path]?.[method]
 
-    if (handler) {
-        handler(request, response)
-    } else {
-        response.json(response.locals.data)
-    }
+  if (handler) {
+    handler(request, response)
+  } else {
+    response.json(response.locals.data)
+  }
 }
 
 const port = 777;
 
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 server.use(bodyParser.json());
@@ -77,6 +82,6 @@ server.use([...middlewares]);
 server.use(router);
 
 server.listen(port, () => {
-    console.log(`mock api is stared on port ${port}`);
+  console.log(`mock api is stared on port ${port}`);
 });
 
